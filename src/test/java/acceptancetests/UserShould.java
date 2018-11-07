c15a6dac00c3bb0f5651acf82cd49a650d966cea
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-07 20:34:16 +0000

    Add setup test

--- src/test/java/acceptancetests/UserShould.java
+++ src/test/java/acceptancetests/UserShould.java
@@ -1,2 +1,13 @@
+package acceptancetests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class UserShould {
+
+    @Test
+    public void sanityCheck() {
+        assertThrows(AssertionError.class, () -> { assert false; });
+    }
 }
